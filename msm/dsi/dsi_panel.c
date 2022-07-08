d45caf4fddce588b416b2fc9156dea913bc456b1
daiss1 <daiss1@lenovo.com>
2022-07-08 10:58:12 +0800

    display: Modify display drive mipi lp11 and reset sequence
    Modify:
    Modify display drive mipi lp11 and reset sequence
    1.8V-->3.3-->LP11-->reset
    
    Change-Id: Ib06f7ecdf5f900b51a29e5c038fd9a2da2aead01
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2326607
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -535,6 +535,8 @@ static int dsi_panel_power_on(struct dsi_panel *panel)
 		DSI_ERR("[%s] failed to set pinctrl, rc=%d\n", panel->name, rc);
 		goto error_disable_vregs;
 	}
+	if (panel->lp11_init)
+		goto  exit;
 	pr_info("[drm] dsi_panel_reset\n");
 	rc = dsi_panel_reset(panel);
 	if (rc) {
@@ -5336,8 +5338,7 @@ int dsi_panel_pre_prepare(struct dsi_panel *panel)
 	mutex_lock(&panel->panel_lock);
 
 	/* If LP11_INIT is set, panel will be powered up during prepare() */
-	if (panel->lp11_init)
-		goto error;
+
 
 	rc = dsi_panel_power_on(panel);
 	if (rc) {
@@ -5494,7 +5495,7 @@ int dsi_panel_prepare(struct dsi_panel *panel)
 	mutex_lock(&panel->panel_lock);
 
 	if (panel->lp11_init) {
-		rc = dsi_panel_power_on(panel);
+		rc = dsi_panel_reset(panel);
 		if (rc) {
 			DSI_ERR("[%s] panel power on failed, rc=%d\n",
 			       panel->name, rc);
